96f355a69505ae45858fa4b346f2e8a9af1ce5a2
vignesh <vignesh@AKASH>
2023-01-03 20:15:35 +0530

    First Commit
    Uploading Docs...

---
+++ EmployeeRegistration/src/main/java/com/vignesh/model/Employee.java
@@ -0,0 +1,57 @@
+package com.vignesh.model;
+
+public class Employee {
+	
+
+private String firstname;
+private String lastname;
+private String username;
+private String password;
+private String location;
+private String contact;
+private String salary;
+
+
+public String getFirstname() {
+	return firstname;
+}
+public void setFirstname(String firstname) {
+	this.firstname = firstname;
+}
+public String getLastname() {
+	return lastname;
+}
+public void setLastname(String lastname) {
+	this.lastname = lastname;
+}
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getLocation() {
+	return location;
+}
+public void setLocation(String location) {
+	this.location = location;
+}
+public String getContact() {
+	return contact;
+}
+public void setContact(String contact) {
+	this.contact = contact;
+}
+public String getSalary() {
+	return salary;
+}
+public void setSalary(String salary) {
+	this.salary = salary;
+}
+}
